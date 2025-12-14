4fe4de2786d1463b4a3cf48bae0dbc0d733ead7d
loopystack19 <sanjaygreg19@gmail.com>
2025-12-14 13:10:44 +0300

    I have decided to use javascript as the main programming language for solving leetcode problems, we going to start with the array data structure

---
+++ Arrays/twoSum.js
@@ -0,0 +1,2 @@
+//In this leetcode problem, you are given a particular array and a target, you browse through all the elements of the array and check if they are any two elements that can add up to give you the target, if so you return the indexes else you return an empty array
+
